e33cf933b89380b64d36fa6ccf2a1c83e239be75
Jesúsfc <jfcaraballo@gmail.com>
2021-04-17 21:38:05 +0200

    Add Index Page and Controller for that one, closes #2.

---
+++ spring-pet-clinic-web/src/main/java/toguru/jesusfc/springpetclinic/Controllers/IndexController.java
@@ -0,0 +1,19 @@
+package toguru.jesusfc.springpetclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created By Jesús Fdez. Caraballo on 17/04/2021.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = {"", "/", "index", "index.html"}, method = RequestMethod.GET)
+    public String goIndexPage(Model model) {
+        return "index";
+    }
+
+}
