b001f2caa52ba16056788049c24b2a9d03ff4f9a
Jesúsfc <jfcaraballo@gmail.com>
2021-04-17 19:22:39 +0200

    Refactoring to common CRUD interface.

---
+++ spring-pet-clinic-data/src/main/java/toguru/jesusfc/springpetclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package toguru.jesusfc.springpetclinic.services;
+
+import java.util.Set;
+
+/**
+ * Created By Jesús Fdez. Caraballo on 17/04/2021.
+ * My Custom CRUD service
+ */
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+    T findById(ID id);
+    T save (T obj);
+    void delete(T obj);
+
+}
