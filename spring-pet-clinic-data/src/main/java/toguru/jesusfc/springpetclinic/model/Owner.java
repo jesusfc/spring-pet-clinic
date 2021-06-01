7a9bf1ded272a866a99689324a39f33877415eb7
jesusfc <jfcaraballo@gmail.com>
2021-06-01 10:33:49 +0200

    Update Project

--- spring-pet-clinic-data/src/main/java/toguru/jesusfc/springpetclinic/model/Owner.java
+++ spring-pet-clinic-data/src/main/java/toguru/jesusfc/springpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package toguru.jesusfc.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created By Jesús Fdez. Caraballo on 12/04/2021.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
--- spring-pet-clinic-data/src/main/java/toguru/jesusfc/springpetclinic/model/Vet.java
+++ spring-pet-clinic-data/src/main/java/toguru/jesusfc/springpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package toguru.jesusfc.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created By Jesús Fdez. Caraballo on 12/04/2021.
  */
 public class Vet extends Person {
+
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
